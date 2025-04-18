43d7f9006e1335459afadca19752bdc7dd4a8f63
Lorenzo <desimone.lorenzo@protonmail.com>
2025-04-18 15:14:40 +0200

    finish up and make pretty

--- utm.sh
+++ utm.sh
@@ -11,13 +11,7 @@
 # todo: add courses array
 # todo: -l list courses +VMs
 
-# CSIN VMs // not needed anymore
-_CL1="LAB-CL1-CSIN"
-_DC1="LAB-DC1-CSIN"
-
-# BMBs VMs // not needed anymore
-_KALI1="kali-2025-W15_HF-BMBS"
-
+# Declare VMs and Courses Array
 declare -a _IFA_VMS
 declare -a _IFA_COURSES
 
@@ -29,36 +23,34 @@ while IFS= read -r line
 		fi
 done < <(utmctl list | sed '1d' | awk '{print $3}')
 
-#echo "${_IFA_VMS[*]}"
-
 
 # GETOPTS STUFF
-#_OPTSTRING="a:f:h"
+_OPTSTRING="a:f:h"
 
-#while getopts ${_OPTSTRING} _FACH; do
-#	case "${_FACH}" in
-#		a)
-#			_ACTION_OPT="${OPTARG}"
-#			;;
-#		f)
-#			_FACH_OPT="${OPTARG}"
-#			;;
-#		h)
-#			echo "Usage: utm [-a action] [-f <fach>] [-h]"
-#			exit 0
-#			;;
-#		\?)
-#			echo "Invalid option: -${OPTARG}" >&2
-#			exit 1
-#			;;
-#		:)
-#			echo "Option -${OPTARG} requires an arugment" >&2
-#			exit 1
-#			;;
-#esac
-#done
+while getopts ${_OPTSTRING} _FACH; do
+	case "${_FACH}" in
+		a)
+			_ACTION_OPT="${OPTARG}"
+			;;
+		f)
+			_FACH_OPT="${OPTARG}"
+			;;
+		h)
+			echo "Usage: utm [-a action] [-f <fach>] [-h]"
+			exit 0
+			;;
+		\?)
+			echo "Invalid option: -${OPTARG}" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -${OPTARG} requires an arugment" >&2
+			exit 1
+			;;
+esac
+done
 
-# Functions to operate VMs for chosen Fach 
+# Function to operate VMs for chosen Fach 
 function operateVMS {
 	local _action_opt=${1}
 	local _fach_opt=${2}
@@ -66,32 +58,10 @@ function operateVMS {
 	for i in "${_IFA_VMS[@]}"
 	do
 		if [[ "${i: -4}" == "${_fach_opt}" ]]; then
-			# utmctl "${_action_opt}" "${i}"
 			echo  "utmctl ${_action_opt} ${i}"
 		fi
 	done
 }
-_ACTION_OPT="start"
-_FACH_OPT="CSIN"
-
-operateVMS "${_ACTION_OPT}" "${_FACH_OPT}"
-
-# Functions to operate VMs for chosen Fach // not needed anymore
-#function operateCSIN {
-#	utmctl "${_ACTION_OPT}" "${_CL1}"
-#	utmctl "${_ACTION_OPT}" "${_DC1}"	
-#}
-
-#function operateBMBS {
-#	utmctl "${_ACTION_OPT}" "${_KALI1}"
-#}
-
 
 # Main Script
-#if [[ "${_FACH_OPT}" == "CSIN" ]]; then
-#	operateCSIN
-#fi
-
-#if [[ "${_FACH_OPT}" == "BMBS" ]]; then
-#	operateBMBS
-#fi
+operateVMS "${_ACTION_OPT}" "${_FACH_OPT}"
